a8d46db1e11f6eb17dd9e738bd7d174fb2d37588
Skerper <31716517+Skerper@users.noreply.github.com>
2020-01-14 15:33:38 +0900

    add heading comments

--- com/skerper/hxvibe/HxVibe.cpp
+++ com/skerper/hxvibe/HxVibe.cpp
@@ -1,6 +1,9 @@
 #include "HxVibe.h"
 
-
+/**
+ * Attempts to vibrate the controller at the given index. Doesn't care if it works or not.
+ * @author Elliott Smith [Skerper]
+ */
 void vibrate(int left, int right, int index)
 {
 	XINPUT_VIBRATION Vibration;
--- com/skerper/hxvibe/HxVibe.hx
+++ com/skerper/hxvibe/HxVibe.hx
@@ -1,5 +1,10 @@
 package com.skerper.hxvibe;
 
+
+/**
+ * Extern to talk to the C++ code
+ * @author Elliott Smith [Skerper]
+ */
 @:include("./HxVibe.cpp")
 extern class HxVibe{
     @:native("vibrate") static function vibrate(left:Int, right:Int, index:Int = 1):Void;
